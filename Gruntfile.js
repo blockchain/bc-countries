e38b84ebd15866271c6ef08a68b3e2a78250bdea
Abdullah Alansari <ahimta@gmail.com>
2015-10-10 13:22:50 +0300

    chore(Gruntfile): remove grunt-contrib-copy

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,7 +42,6 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-karma');
   grunt.loadNpmTasks('grunt-bump');
